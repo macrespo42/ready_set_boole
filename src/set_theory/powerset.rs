c3d6262e84377c83851047aebb1e96b7472fd182
macrespo42 <macrespo@student.42.fr>
2023-04-19 18:25:08 +0200

    fix: refacto variable in powerset

--- src/set_theory/powerset.rs
+++ src/set_theory/powerset.rs
@@ -2,11 +2,11 @@ fn powerset(set: &[i32]) -> Vec<Vec<i32>> {
     let mut result:Vec<Vec<i32>> = Vec::new(); 
     let mut powerset_size: u32 = 2;
     powerset_size = powerset_size.pow(set.len() as u32);
-    for counter in 0..powerset_size {
+    for subset_elt in 0..powerset_size {
         let mut subset: Vec<i32> = Vec::new();
-        for index in 0..set.len() {
-            if counter & (1 << index) > 0 {
-               subset.push(index as i32);
+        for elt in 0..set.len() {
+            if subset_elt & (1 << elt) > 0 {
+               subset.push(elt as i32);
             }
         }
         result.push(subset);
