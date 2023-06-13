6d6b74a820c493b197a9ce8653cf78c94b0edf84
macrespo42 <macrespo@student.42.fr>
2023-06-13 07:38:49 -0400

    feat: ex011 done ✅

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello world");
+    println!("ready... set... boole! 🏁 Choose the exercice you want to check:");
+    println!("Available:");
+    for n in 0..12 {
+        if n < 10 {
+            println!(" - Exercice 0{n}");
+        } else {
+            println!(" - Exercice {n}");
+        }
+    }
 }
