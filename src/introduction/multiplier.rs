1119927ed9828fc283813817480bcb5c9403d2b6
macrespo42 <macrespo@student.42.fr>
2023-06-15 23:25:33 +0200

    fix: multiplier replace modulo by bitwise operation

--- src/introduction/multiplier.rs
+++ src/introduction/multiplier.rs
@@ -8,7 +8,7 @@ pub fn multiplier(a: u32, b: u32) -> u32 {
     let mut d: u32 = b;
     let mut result: u32 = 0;
     while d > 0 {
-        if d % 2 != 0 {
+        if d & 1 == 1 {
             result = adder::adder(result, c);
         }
         c = c << 1;
